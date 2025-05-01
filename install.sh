b9c071d3619291d07773f88c8dd349c8ee22cd27
jeanfraga33 <jean.fraga33@gmail.com>
2025-04-30 23:13:52 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
-#!/bin/bash
-set -e
-
 echo "=== Instalando dependências ==="
 apt update
 apt install -y golang git
 
-
-echo "=== instalando ou atualizando ==="
+echo "=== Instalando ou atualizando repositório ==="
 INSTALL_DIR="/opt/proxyeuro"
 
 if [ -d "$INSTALL_DIR/.git" ]; then
@@ -22,17 +18,23 @@ else
     git clone https://github.com/jeanfraga33/proxy-go2.git "$INSTALL_DIR"
 fi
 
-
 echo "=== Gerando certificados TLS autoassinados ==="
 cd "$INSTALL_DIR"
 openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 700 -nodes -subj "/CN=localhost"
 
+echo "=== Verificando módulo Go ==="
+if [ ! -f "go.mod" ]; then
+    echo "Inicializando módulo Go"
+    go mod init proxyeuro
+    go mod tidy
+fi
+
 echo "=== Compilando proxy ==="
-go build -o /usr/local/bin/proxyeuro proxy_manager.go
+go build -o /usr/local/bin/proxyeuro
 
 echo "=== Limpando cache DNS ==="
 systemd-resolve --flush-caches || resolvectl flush-caches || echo "Não foi possível limpar o cache DNS"
 
 echo "=== Instalação concluída com sucesso ==="
 echo "Use o comando 'proxyeuro' para iniciar"
-echo " Proxy Verção 1.0"
+echo "Proxy Versão 1.0"

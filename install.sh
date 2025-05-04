031d805134cbde4cf0a80a0d33079ce63e80e483
jeanfraga33 <jean.fraga33@gmail.com>
2025-05-04 16:37:57 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,11 @@ rm -rf /opt/proxyeuro
 echo "=== Clonando repositório ==="
 git clone https://github.com/jeanfraga33/proxy-go2.git /opt/proxyeuro
 
-echo "=== Gerando certificados TLS ==="
 cd /opt/proxyeuro
-openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 700 -nodes -subj "/CN=localhost"
+
+echo "=== Gerando certificados TLS ==="
+mkdir -p /opt/proxyeuro/certs
+openssl req -x509 -newkey rsa:2048 -keyout /opt/proxyeuro/certs/key.pem -out /opt/proxyeuro/certs/cert.pem -days 700 -nodes -subj "/CN=localhost"
 
 echo "=== Inicializando módulo Go ==="
 go mod init proxyeuro || echo "go.mod já existe"
@@ -45,6 +47,7 @@ After=network.target
 ExecStart=/usr/local/bin/proxy_worker %i
 Restart=always
 WorkingDirectory=/opt/proxyeuro
+Environment="CERT_DIR=/opt/proxyeuro/certs"
 StandardOutput=file:/var/log/proxyeuro_%i.log
 StandardError=journal
 

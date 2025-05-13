405885f8dd955c944fee0b8ea55b1c2226c2946d
jeanfraga33 <jean.fraga33@gmail.com>
2025-05-13 17:20:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Nome do script Python
-SCRIPT_NAME="proxy_server.py"
-SCRIPT_PATH="/usr/local/bin/$SCRIPT_NAME"
+SCRIPT_NAME="proxy-py.py"
+SCRIPT_PATH="/usr/local/bin/proxy_server.py"
 
 # Função para remover instalações anteriores
 remove_previous_installation() {
@@ -27,7 +27,7 @@ install_dependencies() {
 clone_repository() {
     echo "Clonando o repositório do GitHub..."
     git clone https://github.com/jeanfraga33/proxy-go2.git
-    cp "proxy-go2/$SCRIPT_NAME" "$SCRIPT_PATH"
+    cp "proxy-go2/proxy-py.py" "$SCRIPT_PATH"
     chmod +x "$SCRIPT_PATH"
     echo "Script instalado em $SCRIPT_PATH."
 }
@@ -37,7 +37,7 @@ main() {
     remove_previous_installation
     install_dependencies
     clone_repository
-    echo "Instalação concluída. Você pode executar o proxy usando o comando: $SCRIPT_NAME"
+    echo "Instalação concluída. Você pode executar o proxy usando o comando: proxy_server.py"
 }
 
 # Executa a função principal
